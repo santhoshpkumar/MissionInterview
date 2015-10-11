83d03e18a8d5f1144282c8239663d366c7b119b0
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-11 21:23:21 +0530

    Longest common sub-sequence implementation

--- src/org/santhoshkumar/DynamicProgramming/LongestCommonSubSequence.java
+++ src/org/santhoshkumar/DynamicProgramming/LongestCommonSubSequence.java
@@ -4,4 +4,41 @@ package org.santhoshkumar.DynamicProgramming;
  * Created by santhoshkumar on 11/10/15.
  */
 public class LongestCommonSubSequence {
+
+    public int findRecursive(String one, String two){
+        if(one.length()==0 || two.length()==0){
+            return 0;
+        }
+
+        if(one.charAt(0)==two.charAt(0)){
+            return 1+findRecursive(one.substring(1),two.substring(1));
+        }else{
+            return Math.max(findRecursive(one,two.substring(1)), findRecursive(one.substring(1),two));
+        }
+    }
+
+    public int findLongest(String one, String two){
+        int[][] matrix = new int[one.length()+1][two.length()+1];
+
+        int oneLength = one.length();
+        int twoLength = two.length();
+
+        for(int i = 0; i <= oneLength; i++){
+            matrix[i][0]=0;
+        }
+        for(int i = 0; i <= twoLength; i++){
+            matrix[0][i]=0;
+        }
+
+        for(int i =1; i <=oneLength; i++){
+            for(int j =1; j <=twoLength; j++){
+                if(one.charAt(i-1)==two.charAt(j-1)){
+                    matrix[i][j]=1+matrix[i-1][j-1];
+                }else{
+                    matrix[i][j]=Math.max(matrix[i][j-1],matrix[i-1][j]);
+                }
+            }
+        }
+        return matrix[oneLength][twoLength];
+    }
 }
--- src/org/santhoshkumar/Main.java
+++ src/org/santhoshkumar/Main.java
@@ -1,5 +1,6 @@
 package org.santhoshkumar;
 
+import org.santhoshkumar.DynamicProgramming.LongestCommonSubSequence;
 import org.santhoshkumar.DynamicProgramming.LongestPalindromicSubSequence;
 
 public class Main {
@@ -8,5 +9,9 @@ public class Main {
         LongestPalindromicSubSequence lonPalindrome = new LongestPalindromicSubSequence();
         System.out.println(lonPalindrome.findRecursive(new String("AABCDEBAZ")));
         System.out.println(lonPalindrome.findPalindrome(new String("AABCDEBAZ")));
+
+        LongestCommonSubSequence lonComSeq = new LongestCommonSubSequence();
+        System.out.println(lonComSeq.findRecursive(new String("ABCD"), new String("ACD")));
+        System.out.println(lonComSeq.findLongest(new String("ABCD"), new String("AD")));
     }
 }
