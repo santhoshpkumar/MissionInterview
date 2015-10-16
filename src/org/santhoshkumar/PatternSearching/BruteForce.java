f40788691fb750a6af2b2e92bd71753ae225b246
Santhosh Kumar Prakash <santhosh.prakash@gmail.com>
2015-10-16 08:09:24 +0530

    Brute force pattern searching

---
+++ src/org/santhoshkumar/PatternSearching/BruteForce.java
@@ -0,0 +1,24 @@
+package org.santhoshkumar.PatternSearching;
+
+/**
+ * Created by sk010962 on 10/16/2015.
+ */
+public class BruteForce {
+    public int find(String givenString, String pattern){
+        int strLength = givenString.length();
+        int patLength = pattern.length();
+
+        for(int i =0; i < strLength; i++){
+            int j = 0;
+            for(; j < patLength; j++){
+                if(givenString.charAt(i+j)!=pattern.charAt(j)){
+                    break;
+                }
+            }
+            if(j == patLength){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
