65af5e9c1d658301821f2d193e9eeb9e061e09a9
Santhosh Kumar Prakash <santhosh.prakash@gmail.com>
2015-10-16 08:18:18 +0530

    Brute force pattern searching

--- src/org/santhoshkumar/PatternSearching/BruteForce.java
+++ src/org/santhoshkumar/PatternSearching/BruteForce.java
@@ -3,7 +3,9 @@ package org.santhoshkumar.PatternSearching;
 /**
  * Created by sk010962 on 10/16/2015.
  *
- * Note: Brute-force algorithm needs backup for every mismatch.
+ * Constraint: Brute-force algorithm needs backup for every mismatch.
+ * Solution: 1. Use pattern size buffer buffer
+ *           2. Use better algorithm
  *
  */
 public class BruteForce {
@@ -22,6 +24,25 @@ public class BruteForce {
                 return i;
             }
         }
-        return -1;
+        return -1; // Pattern not found
+    }
+
+    // Alternate explicit backup
+    public int find2(String givenString, String pattern) {
+        int i, strLength = givenString.length();
+        int j, patLength = pattern.length();
+
+        for (i = 0, j = 0; i < strLength && j < patLength; i++, j++) {
+            if (givenString.charAt(i + j) != pattern.charAt(j)) {
+                break;
+            } else {
+                i -= j; // backup i to matched so far
+                j = 0; // reset j = zero
+            }
+        }
+        if (j == patLength) {
+            return i;
+        }
+        return -1; // Pattern not found
     }
 }
