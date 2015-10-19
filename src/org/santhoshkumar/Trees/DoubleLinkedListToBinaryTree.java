e3dadb3e2867f554c12a2cb9993bcf04373a69f7
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-19 07:47:43 +0530

    Double Linked List to Binary Tree in O(n)

--- src/org/santhoshkumar/Trees/DoubleLinkedListToBinaryTree.java
+++ src/org/santhoshkumar/Trees/DoubleLinkedListToBinaryTree.java
@@ -4,6 +4,45 @@ package org.santhoshkumar.Trees;
  * Created by santhoshkumar on 19/10/15.
  */
 public class DoubleLinkedListToBinaryTree {
+    DLLNode head;
+    DLLNode tail;
+
+    int size;
+
+    public void add(int data){
+        DLLNode node = new DLLNode(data);
+        if(head == null) {
+            head = node;
+            tail = node;
+        }else{
+            head.prev = node;
+            node.next = head;
+            head = node;
+        }
+        size++;
+    }
+
+    public void inOrder(DLLNode root){
+        if(root == null){
+            return;
+        }
+        inOrder(root.prev); //prev = left
+        System.out.print(root.data+ " ");
+        inOrder(root.next); //next = right
+    }
+
+    public DLLNode convert(int size){
+        if(size == 0){
+            return null;
+        }
+        DLLNode left = convert(size/2);
+        DLLNode root = head;
+        root.prev = left;
+        head = head.next;
+        root.next = convert(size-((size/2)-1));
+        return root;
+    }
+
 }
 
 // Doubly Linked List Node.
@@ -12,4 +51,7 @@ class DLLNode {
     DLLNode next;
     DLLNode prev;
     int data;
+    DLLNode(int data){
+        this.data = data;
+    }
 }
