d58d859e370bd540f0fdf06482b0cb7db5403dd2
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-19 07:30:50 +0530

    Double Linked List to Binary Tree in O(n)

---
+++ src/org/santhoshkumar/Trees/DoubleLinkedListToBinaryTree.java
@@ -0,0 +1,15 @@
+package org.santhoshkumar.Trees;
+
+/**
+ * Created by santhoshkumar on 19/10/15.
+ */
+public class DoubleLinkedListToBinaryTree {
+}
+
+// Doubly Linked List Node.
+
+class DLLNode {
+    DLLNode next;
+    DLLNode prev;
+    int data;
+}
