9b523edf297c982c885b445eda93f0c10e658217
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-21 17:42:29 +0530

    Is BST and Is Balanced

---
+++ src/org/santhoshkumar/Trees/isBalanced.java
@@ -0,0 +1,9 @@
+package org.santhoshkumar.Trees;
+
+/**
+ * Created by santhoshkumar on 21/10/15.
+ *
+ * Determine if the binary tree is balanced
+ */
+public class isBalanced {
+}
---
+++ src/org/santhoshkumar/Trees/isBinarySearchTree.java
@@ -0,0 +1,8 @@
+package org.santhoshkumar.Trees;
+
+/**
+ * Created by santhoshkumar on 21/10/15.
+ * Determine if the Binary Tree is Binary Search Tree
+ */
+public class isBinarySearchTree {
+}
